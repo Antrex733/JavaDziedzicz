3511de33cb55084df8423781822d5756bad1105c
Antoni.N <antoni@antrex.com>
2022-11-15 13:06:23 +0100

    JavaDziedziczenie3

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,6 @@ public class Main {
         animals[4] = new Bird("Sikor", 1, 0.2, "yellow");
         animals[5] = new Dog(4, 6);
 
-        animals[0] = new Blowfish(53, 2);
-        animals[1] = new Dog("Piorun", 7, 12.4, "Husky", 4);
-        animals[2] = new Fish("smaller fishes", 2);
-        animals[3] = new Pingeon(1.2, 4);
-        animals[4] = new Bird("Sikor", 1, 0.2, "yellow");
-        animals[5] = new Dog(4, 6);
-
         for (Animal a:animals)
             a.getAge();
 
